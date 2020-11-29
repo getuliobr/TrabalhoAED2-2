77cbc843e30647ee1b8c0aa1f63af0c51a89698b
Nuisigor <dexchanel@gmail.com>
2020-11-29 15:43:26 -0300

    Comentários no programa

--- Modules/TRIE/trie.c
+++ Modules/TRIE/trie.c
@@ -10,13 +10,13 @@ static int mappedChar(int chr) {
     return chr % 'a';
 }
 
-static void toLower(char* str) {
+static void toLower(char* str) {  // Transforma a palavra para letra minuscula
     for(int i = 0; str[i]; i++){
         str[i] = tolower(str[i]);
     }
 }
 
-static TRIE* AT_Criar(){
+static TRIE* AT_Criar(){        
     TRIE* noh = malloc(sizeof(TRIE));
     int i;
     noh->estado = ATE_LIVRE;
@@ -52,7 +52,7 @@ static void inserirDic(TRIE** T, char* arq_lista_palavras) {
     int linha = 1;
     FILE* fp = fopen(arq_lista_palavras, "r");
     int maiorPalavra = 0;
-    while (fgets (palavra, sizeof(palavra), fp)) {
+    while (fgets (palavra, sizeof(palavra), fp)) { //Busca cada palavra do dicionario e insere na Trie
         char buffer[MAXC] = "";
 
         if (linha == 1) {
@@ -102,16 +102,16 @@ void Busca_Palavra_Prefixo(TRIE *T, char* prefix, Lista* retorno){
     int i;
     if(T != NULL){
         if(T->estado == ATE_OCUPADO) {
-            lista_inserir_fim(retorno, prefix);
+            lista_inserir_fim(retorno, prefix); // Caso ache uma palavra com o prefixo recebido insere na lista chamada por parametro
         }
 
-        int tam_prefix = strlen(prefix);
-        char* aux = (char*) malloc(tam_prefix + 2);
+        int tam_prefix = strlen(prefix);            
+        char* aux = (char*) malloc(tam_prefix + 2); // Cria uma copia do prefixo com 1 letra a mais
         strcpy(aux, prefix);
 
         for(i = 0; i < 26; i++){
-            if(T->filhos[i] != NULL){
-                aux[tam_prefix] = 'a'+i;
+            if(T->filhos[i] != NULL){               // Procura as letras seguintes do prefixo
+                aux[tam_prefix] = 'a'+i;            // Caso ache, coloca a letra seguinte na cópia do prefixo e procura a próxima
                 aux[tam_prefix+1] = '\0';
                 Busca_Palavra_Prefixo(T->filhos[i], aux, retorno);
             }
@@ -120,16 +120,17 @@ void Busca_Palavra_Prefixo(TRIE *T, char* prefix, Lista* retorno){
     }
 }
 
-Lista* TRIE_ChavesComPrefixo(TRIE * T, char* prefix){
+Lista* TRIE_ChavesComPrefixo(TRIE * T, char* prefix){   
     int i;
-    int tam = strlen(prefix);
+    int tam = strlen(prefix);           
     TRIE* aux = T;
     Lista* palavras = lista_criar();
-    char* strAux = (char*) malloc(strlen(prefix) + 1);
+    char* strAux = (char*) malloc(strlen(prefix) + 1);      //Cria uma copia do prefixo
     strcpy(strAux, prefix);
+
     for(i = 0; i < tam; i++)
-        aux = aux->filhos[mappedChar(prefix[i])];
-    Busca_Palavra_Prefixo(aux, strAux, palavras);
+        aux = aux->filhos[mappedChar(prefix[i])]; // Redireciona a Trie para onde está a ultima letra do prefixo  
+    Busca_Palavra_Prefixo(aux, strAux, palavras); // Procura palavras com o prefixo
 
     return palavras;
 }
@@ -137,8 +138,8 @@ Lista* TRIE_ChavesComPrefixo(TRIE * T, char* prefix){
 void Busca_Palavras_que_Casam(TRIE* T, char* padrao, int nivel, int sem_extra, Lista* l){
     int i;
     if(T != NULL){
-        if(nivel > strlen(padrao)) return;
-        if(nivel > sem_extra && nivel <= strlen(padrao) && T->estado == ATE_OCUPADO) {
+        if(nivel > strlen(padrao)) return; // Condição de parada caso a busca ultrapasse o tamanho da palavra + n_extras
+        if(nivel > sem_extra && nivel <= strlen(padrao) && T->estado == ATE_OCUPADO) { // Insere a palavra na lista caso ache
             char* toInsert = (char*) malloc(nivel + 1);
             memcpy(toInsert, padrao, nivel);
             toInsert[nivel] = '\0';
@@ -146,14 +147,14 @@ void Busca_Palavras_que_Casam(TRIE* T, char* padrao, int nivel, int sem_extra, L
             free(toInsert);
         }
 
-        char *aux = malloc(strlen(padrao)+1);
-        strcpy(aux, padrao);
+        char *aux = malloc(strlen(padrao)+1);               //Cria cópia da chave
+        strcpy(aux, padrao);                
         aux[strlen(padrao)] = '\0';
 
-        if(padrao[nivel] != '*'){
+        if(padrao[nivel] != '*'){   //Caso não seja o coringa, procura a letra seguinte
             Busca_Palavras_que_Casam(T->filhos[mappedChar(padrao[nivel])], aux, nivel+1, sem_extra, l);
         }
-        else{
+        else{                       //Caso seja o coringa procura em todas as 26 chaves
             for(i = 0; i < 26; i++){
                 if(T->filhos[i] != NULL){
                     aux[nivel] = 'a'+i;
@@ -170,8 +171,8 @@ Lista* TRIE_ChavesQueCasam(TRIE *T, char* padrao, int n_extras){
     int i;
     int tam = n_extras == 0 ? strlen(padrao)+1: strlen(padrao)+n_extras+1;
     char* aux = malloc(tam);
-    strcpy(aux, padrao);
-    for(i = strlen(padrao); i < tam; i++){
+    strcpy(aux, padrao);    
+    for(i = strlen(padrao); i < tam; i++){  //Insere coringas extras na palavra 
         aux[i] = '*';
     }
     aux[tam-1] = '\0';
@@ -188,14 +189,14 @@ char* TRIE_ChaveMaiorPrefixoDe(TRIE* T, char* s){
     maiorChave[0] = '\0';
     TRIE* aux = T;
     for(int i = 0; i < tam; i++) {
-        if(aux->estado == ATE_OCUPADO) lista_inserir_fim(chaves, maiorChave);
+        if(aux->estado == ATE_OCUPADO) lista_inserir_fim(chaves, maiorChave); // Insere todos os prefixos encontrados até o final
         if(aux->filhos[mappedChar(s[i])] == NULL) break;
         maiorChave[currPos++] = s[i];
         maiorChave[currPos] = '\0';
         aux = aux->filhos[mappedChar(s[i])];
     }
-    if(chaves->ultimo == NULL) return NULL;
-    char* toCopy = (char*)chaves->ultimo->dado;
+    if(chaves->ultimo == NULL) return NULL;         // Caso não encontre nenhum retorna NULL
+    char* toCopy = (char*)chaves->ultimo->dado;     // Caso encontre, retorna o ultimo elemento inserido da lista
     strcpy(maiorChave, toCopy);
     lista_destruir(chaves);
     return maiorChave;
@@ -205,15 +206,15 @@ static void inserir_dic(TRIE** dicionario, Lista* l){
     int j = l->qtde;
     char* aux;
     for(int i = 0; i < j; i++){
-        lista_remover2(l, 0, &aux);
-        AT_Inserir(dicionario, aux, i);
+        lista_remover2(l, 0, &aux);         // Remove a primeira palavra da lista
+        AT_Inserir(dicionario, aux, i);     // Insere no dicionario
         free(aux);
     }
 }
 
 static void CorrigirOrtografia_Regra1(TRIE* dicionario, TRIE* entrada, char* palavra) {
     char* aux = malloc(strlen(palavra));
-    for(int i = 0; i < strlen(palavra); i++){
+    for(int i = 0; i < strlen(palavra); i++){ // Troca letra por letra pelo coringa e faz a busca em cada caso retornando uma TRIE
         strcpy(aux, palavra);
         aux[i] = '*';
         Lista* listaAux = TRIE_ChavesQueCasam(dicionario, aux, 0);
@@ -231,20 +232,21 @@ static void CorrigirOrtografia_Regra2(TRIE* dicionario, TRIE* entrada, char* pal
     char* aux = malloc(n);
     strcpy(aux, palavra);
     aux[n - 3] = '\0';
-    Lista* out = TRIE_ChavesComPrefixo(dicionario, aux);
+    Lista* out = TRIE_ChavesComPrefixo(dicionario, aux); // Procura os prefixos
     free(aux);
-    inserir_dic(&entrada, out);
+    inserir_dic(&entrada, out); // Insere na trie passada por parametro
     lista_destruir(out);
 }
 
 static void CorrigirOrtografia_Regra3(TRIE* dicionario, TRIE* entrada, char* palavra) {
-    char* out = TRIE_ChaveMaiorPrefixoDe(dicionario, palavra);
+    char* out = TRIE_ChaveMaiorPrefixoDe(dicionario, palavra); // Encontra o maior prefixo
     if(out == NULL) return;
-    AT_Inserir(&entrada, out, entrada->tamanho);
+    AT_Inserir(&entrada, out, entrada->tamanho);// Insere na trie passada por parametro
     free(out);
 }
 
 static Lista* CorrigirOrtografia_Regra4(TRIE* dicionario, TRIE* entrada, char* palavra) {
+    //Junção da regra 1 e 3
     TRIE* novaTrie = AT_Criar();
     CorrigirOrtografia_Regra1(dicionario, novaTrie, palavra);
     CorrigirOrtografia_Regra3(dicionario, novaTrie, palavra);
@@ -252,14 +254,14 @@ static Lista* CorrigirOrtografia_Regra4(TRIE* dicionario, TRIE* entrada, char* p
     return TRIE_ChavesQueCasam(novaTrie, "*", MAXC);
 }
 
-static int checarFiltro(char ch, char* filtro) {
+static int checarFiltro(char ch, char* filtro) { // Função para retirar pontuações do texto
     for(int i = 0; i < strlen(filtro); i++)
         if(ch == filtro[i] || ch == EOF)
             return 1;
     return 0;
 }
 
-int noDicionario(TRIE* dicionario, char* palavra) {
+int noDicionario(TRIE* dicionario, char* palavra) { // Checa se a palavra está no dicionario
     TRIE* aux = dicionario;
     int tamanho = strlen(palavra);
     for(int i = 0; i < tamanho; i++) {
@@ -269,7 +271,7 @@ int noDicionario(TRIE* dicionario, char* palavra) {
     return 1;
 }
 
-static void Lista_Organiza(Lista** entrada){
+static void Lista_Organiza(Lista** entrada){ // Organiza a Lista de alfabética e colocando as palavras maiores primeiro
     No* aux;
     int tam = (*entrada)->qtde;
     int proximo;
@@ -314,10 +316,10 @@ void CorrigirOrtografia(TRIE* dicionario, char* texto){
     int pos = 0;
     while (1) {
         ch = fgetc(arq);
-        if(checarFiltro(ch, filtro)) {
+        if(checarFiltro(ch, filtro)) { //Filtra a palavra retirando as pontuações e números
             palavra[pos] = '\0';
             qtd_palavras++;
-            if(!noDicionario(dicionario, palavra)) {
+            if(!noDicionario(dicionario, palavra)) { // Caso a palavra não esteja no dicionario sugere as palavras de acordo com a regra 4
                 TRIE* trieEntrada = AT_Criar();
                 Lista* sugestoes = CorrigirOrtografia_Regra4(dicionario, trieEntrada, palavra);
                 Lista_Organiza(&sugestoes);
@@ -327,7 +329,7 @@ void CorrigirOrtografia(TRIE* dicionario, char* texto){
                 qtd_sugestoes += sugestoes->qtde;
             }
             pos = 0;
-            if(ch == EOF){
+            if(ch == EOF){ // Caso chegue no final do arquivo, imprime os resultados dos testes e fecha a função
                 printf("Total de palavras : %d\n", qtd_palavras);
                 printf("Total de palavras incorretas : %d\n", qtd_palavras_incorretas);
                 printf("Media de Sugestoes por palavra incorreta : %.3f\n", (float)qtd_sugestoes/qtd_palavras_incorretas);
